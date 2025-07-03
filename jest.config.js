51613f247129206b09f504d03245121d26870c50
iamshivanand <gangwar.shiv@gmail.com>
2025-07-04 02:26:04 +0530

    Revert "Attempt to upgrade to Expo SDK 53 and fix Jest tests"
    This reverts commit a3b3c7fabd6fb9a1104e929fe036a94e1dd8e7b7.

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   preset: 'jest-expo',
-  // setupFiles: ["./jest-preload.js"], // Removed
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
   ],
   setupFilesAfterEnv: ["./jest-setup.js"],
-  testEnvironment: 'react-native/jest/react-native-env.js', // Explicitly set to RN's environment
-  clearMocks: true,
+  // Ensure mocks directory is recognized if not at root or if resolution issues occur
+  // roots: ['<rootDir>'], // Default, usually fine
+  // modulePaths: ['<rootDir>'], // Default
 };
